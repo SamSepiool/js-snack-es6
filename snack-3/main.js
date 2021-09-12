10d14ab4086d7167d77a89ea3932e992c0bcb58d
SamSepiool <simone.severini@outlook.it>
2021-09-12 23:46:07 +0200

    add validation

--- snack-3/main.js
+++ snack-3/main.js
@@ -8,20 +8,21 @@ const iDKWhatImDoing = (arr, a, b) => arr.filter ( (element, index) => {return i
 
  const arrTest = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-console.log(arrTest.length + arrTest)
-
-
-
+console.log(arrTest);
 
 
 
 var numB = parseInt(prompt("l'array ha " + arrTest.length + ' elementi.' + ' inserisci un numero non superiore a ' + arrTest.length));
-// while (!num2 <= arrRandom.length){
-//     num2 = parseInt(prompt('attento, devi inserire un numero non superiore a ' + arrRandom.length));
-// }
 
-var numA = parseInt(prompt('ora inserisci un numero inferiore a quello appena inserito' + (numB)));
-// while (!num1 < num2){
-//     num1 = parseInt(prompt('attento, devi inserire un numero inferiore a ' + (num2)));
-// }
+while (numB > arrTest.length || numB == 0 || isNaN(numB)){
+    numB = parseInt(prompt('attento, devi inserire un numero non superiore a ' + arrTest.length));
+}
+
+var numA = parseInt(prompt('ora inserisci un numero inferiore a ' + (numB)));
+
+while (numA >= numB || numA == 0 || isNaN(numA)){
+    numA = parseInt(prompt('attento, devi inserire un numero inferiore a ' + (numB)));
+}
+
+
 console.log(iDKWhatImDoing(arrTest, numA, numB));
